3e5114fd383c306ba769b3228f3d64ae89fa7b2e
jinthebin <abhinav.jindal912@gmail.com>
2024-10-01 07:40:35 +0100

    removed data transformation from app

--- src/app.py
+++ src/app.py
@@ -10,27 +10,6 @@ st.set_page_config(layout="wide", page_title="Health Index - UK")
 st.title("Health across the UK")
 
 
-# #changing directory to parent directory
-# os.chdir("..")
-# print(os.getcwd())
-# #Loading all data
-# health_ind = pd.read_excel("dat/healthindex.xlsx", sheet_name='data')
-# #removing unwanted columns
-# health_ind.drop(columns=["Numerator","Denominator"], axis=1, inplace=True)
-
-# gloassry = pd.read_excel("dat/healthindex.xlsx", 
-#                              sheet_name='Table_1_Indicator_details', 
-#                              header=2,
-#                              usecols="A:J"
-#                              )
-# #Health index england import:
-# exc4 = "dat/healthindexscoresengland.xlsx" #defining path
-# df4 = pd.read_excel(exc4, sheet_name="Table_2_Index_scores", header=2 )
-# df4 = (df4.rename(columns={"Area Type [Note 3]": "Area Type"})) #tranforming dataframe - renaming columns
-
-# #changing directory back to src folder
-# os.chdir("src")
-
 with st.expander("Click here for additional information"):
     st.write("Some really useful information")
     st.link_button(label="Find out more", 
