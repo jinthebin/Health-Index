f3e137650dcd6b0208d1f595d062bd6bf1f045b2
jinthebin <abhinav.jindal912@gmail.com>
2024-10-01 09:29:06 +0100

    added map data

--- src/app.py
+++ src/app.py
@@ -3,7 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import os
-from data_read import health_ind, df4, glossary, exc4
+import folium
+from streamlit_folium import st_folium
+import geopandas
+from data_read import health_ind, hlth_ind_eng, glossary, hlth_ind_eng_gdf, hlth_ind_eng_LTLA_gdf
 
 st.set_page_config(layout="wide", page_title="Health Index - UK")
 
@@ -18,30 +21,83 @@ with st.expander("Click here for additional information"):
     # st.image("https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg")
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(
-    ["UK wide data by year",
+    ["Overall Health Index Scores",
      "Indices by region",
+     "UK wide data by year",
      "Glossary",
-     "Overall Health Index Scores",
      "KPIs"]
      )
 
 with tab1:
+    #need to use selenium to crawl on ONS website:
+    #https://stackoverflow.com/questions/16627227/how-do-i-avoid-http-error-403-when-web-scraping-with-python#:~:text=This%20is%20probably%20because%20of%20mod_security
+    #https://www.freecodecamp.org/news/better-web-scraping-in-python-with-selenium-beautiful-soup-and-pandas-d6390592e251
+    #exc4 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/datasets/healthindexscoresengland/current/healthindexscoresengland.xlsx"
+    # exc4 = "dat/healthindexscoresengland.xlsx"
+    # df4 = pd.read_excel(exc4, sheet_name="Table_2_Index_scores", header=2 )
 
-    st.divider()
+    # df4 = (df4.rename(columns={"Area Type [Note 3]": "Area Type"})) #tranforming dataframe - renaming columns
 
-    year_select = st.selectbox("Please select the year",
-                             health_ind["Year"].unique()
-                             )
-    st.plotly_chart(
-        px.bar(
-            health_ind,
-            x = "Area name",
-            y = "Value",
-            color= "Indicator name",
-            title=f"Health across UK for {year_select}", 
+    areatyp_select = st.selectbox("Please select an Area Type",
+                                hlth_ind_eng["Area Type"].unique()
+                                )
+    hlth_ind_eng = hlth_ind_eng[hlth_ind_eng["Area Type"]==areatyp_select]
 
-        )
+    area_select = st.multiselect("Please select areas",
+                                hlth_ind_eng["Area Name"].unique()
+                                )
+    # if area_select is None:
+    #     area_select = df4["Area Name"].unique()
+    # else:
+
+
+    hlth_ind_eng = hlth_ind_eng[hlth_ind_eng["Area Name"].isin(area_select)]
+    #reverse-pivot the dataframe 
+
+    df = (hlth_ind_eng.melt(id_vars=['Area Code','Area Name','Area Type'], var_name='Year', value_name='Health Index')
+            .sort_values(['Area Name','Year'])
+            .reset_index(drop=True))
+
+    col_1, col_2 = st.columns([0.3, 0.7])
+    with col_1:
+
+        st.dataframe(
+                df,
+                use_container_width=True
+                )
+    with col_2:
+        st.plotly_chart(
+                px.line(df, 
+                            x="Year", 
+                            y="Health Index", 
+                            color="Area Name",
+                            markers=True,
+                            symbol="Area Name",
+                            title=f'Overall Health Index for area type {areatyp_select}, over time')
+            )
+    st.header("Top areas by overall Health index for given year:", divider=True)
+    year_sel = st.selectbox("Please select year",
+                                df["Year"].unique()
+                                )
+    df_top_yr = df[df["Year"]==year_sel]
+    st.dataframe(
+        # df['Health Index'].nlargest(n=10)
+        df_top_yr.nlargest(n=10, columns=['Health Index'], keep='all')
+        
     )
+    #experimental maps for England regions: 
+    st.header("England Regions - EXPERIMENTAL", divider=True)
+    fig, ax = plt.subplots()
+
+    hlth_ind_eng_gdf.plot(
+    column="2021",
+    legend=True,
+    ax=ax
+    )
+
+    st.pyplot(fig)
+
+
 
 with tab2:
 
@@ -83,7 +139,26 @@ with tab2:
                 )
             }
             )
+
 with tab3:
+
+    st.divider()
+
+    year_select = st.selectbox("Please select the year",
+                             health_ind["Year"].unique()
+                             )
+    st.plotly_chart(
+        px.bar(
+            health_ind,
+            x = "Area name",
+            y = "Value",
+            color= "Indicator name",
+            title=f"Health across UK for {year_select}", 
+
+        )
+    )
+    
+with tab4:
     # gloassry = pd.read_excel("dat/healthindex.xlsx", 
     #                          sheet_name='Table_1_Indicator_details', 
     #                          header=2,
@@ -95,64 +170,6 @@ with tab3:
             glossary,
             use_container_width=True
             )
-
-with tab4:
-    #need to use selenium to crawl on ONS website:
-    #https://stackoverflow.com/questions/16627227/how-do-i-avoid-http-error-403-when-web-scraping-with-python#:~:text=This%20is%20probably%20because%20of%20mod_security
-    #https://www.freecodecamp.org/news/better-web-scraping-in-python-with-selenium-beautiful-soup-and-pandas-d6390592e251
-    #exc4 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/datasets/healthindexscoresengland/current/healthindexscoresengland.xlsx"
-    # exc4 = "dat/healthindexscoresengland.xlsx"
-    # df4 = pd.read_excel(exc4, sheet_name="Table_2_Index_scores", header=2 )
-
-    # df4 = (df4.rename(columns={"Area Type [Note 3]": "Area Type"})) #tranforming dataframe - renaming columns
-
-    areatyp_select = st.selectbox("Please select an Area Type",
-                                df4["Area Type"].unique()
-                                )
-    df4 = df4[df4["Area Type"]==areatyp_select]
-
-    area_select = st.multiselect("Please select areas",
-                                df4["Area Name"].unique()
-                                )
-    # if area_select is None:
-    #     area_select = df4["Area Name"].unique()
-    # else:
-
-
-    df4 = df4[df4["Area Name"].isin(area_select)]
-    #reverse-pivot the dataframe 
-
-    df = (df4.melt(id_vars=['Area Code','Area Name','Area Type'], var_name='Year', value_name='Health Index')
-            .sort_values(['Area Name','Year'])
-            .reset_index(drop=True))
-
-    col_1, col_2 = st.columns([0.3, 0.7])
-    with col_1:
-
-        st.dataframe(
-                df,
-                use_container_width=True
-                )
-    with col_2:
-        st.plotly_chart(
-                px.line(df, 
-                            x="Year", 
-                            y="Health Index", 
-                            color="Area Name",
-                            markers=True,
-                            symbol="Area Name",
-                            title=f'Overall Health Index for area type {areatyp_select}, over time')
-            )
-    st.header("Top areas by overall Health index for given year:", divider=True)
-    year_sel = st.selectbox("Please select year",
-                                df["Year"].unique()
-                                )
-    df_top_yr = df[df["Year"]==year_sel]
-    st.dataframe(
-        # df['Health Index'].nlargest(n=10)
-        df_top_yr.nlargest(n=10, columns=['Health Index'], keep='all')
-        
-    )
 
 
 with tab5:
--- src/data_read.py
+++ src/data_read.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import geopandas
 import os
 
 #changing directory to parent directory
@@ -15,9 +16,58 @@ glossary = pd.read_excel("dat/healthindex.xlsx",
                              usecols="A:J"
                              )
 #Health index england import:
-exc4 = "dat/healthindexscoresengland.xlsx" #defining path
-df4 = pd.read_excel(exc4, sheet_name="Table_2_Index_scores", header=2 )
-df4 = (df4.rename(columns={"Area Type [Note 3]": "Area Type"})) #tranforming dataframe - renaming columns
+hlth_ind_eng_pth = "dat/healthindexscoresengland.xlsx" #defining path
+hlth_ind_eng = pd.read_excel(hlth_ind_eng_pth, sheet_name="Table_2_Index_scores", header=2 )
+hlth_ind_eng = (hlth_ind_eng.rename(columns={"Area Type [Note 3]": "Area Type"})) #tranforming dataframe - renaming columns
+
+region_dat = pd.read_csv("dat/Regions_December_2020_EN_BFC_2022.csv", usecols=['RGN20CD','RGN20NM','BNG_E','BNG_N','LONG','LAT'])
+region_dat = (region_dat.rename(columns={"RGN20CD": "Area Code"})) #renaming columns
+region_dat.drop(columns=["RGN20NM"], axis=1, inplace=True) #dropping name region
+
+#merging with health_index_england data:
+hlth_ind_eng_mrg = pd.merge(
+    left=hlth_ind_eng,
+    right=region_dat,
+    left_on='Area Code',
+    right_on='Area Code',
+    how="left"
+)
+
+#converitng to geopandas dataframe
+
+hlth_ind_eng_gdf = geopandas.GeoDataFrame(
+    hlth_ind_eng_mrg, # Our pandas dataframe
+    geometry = geopandas.points_from_xy(
+        hlth_ind_eng_mrg['LONG'], # Our 'x' column (horizontal position of points)
+        hlth_ind_eng_mrg['LAT'] # Our 'y' column (vertical position of points)
+        ),
+    crs = 'EPSG:4326' # the coordinate reference system of the data - use EPSG:4326 if you are unsure
+    )
+
+
+#Reading LTLA point data:
+LTLA_dat = pd.read_csv("dat/GLTLA_DEC_2022_EW_BFC.csv", usecols=['GLTLA22CD','BNG_E','BNG_N','LONG','LAT'])
+LTLA_dat = (region_dat.rename(columns={"GLTLA22CD": "Area Code"})) #renaming columns
+
+#merging with health_index_england data:
+hlth_ind_eng_LTLA_mrg = pd.merge(
+    left=hlth_ind_eng,
+    right=LTLA_dat,
+    left_on='Area Code',
+    right_on='Area Code',
+    how="left"
+)
+
+#converitng to geopandas dataframe
+hlth_ind_eng_LTLA_gdf = geopandas.GeoDataFrame(
+    hlth_ind_eng_LTLA_mrg, # Our pandas dataframe
+    geometry = geopandas.points_from_xy(
+        hlth_ind_eng_LTLA_mrg['LONG'], # Our 'x' column (horizontal position of points)
+        hlth_ind_eng_LTLA_mrg['LAT'] # Our 'y' column (vertical position of points)
+        ),
+    crs = 'EPSG:4326' # the coordinate reference system of the data - use EPSG:4326 if you are unsure
+    )
+
 
 #changing directory back to src folder
 os.chdir("src")
